a8d429abe948e57e64abca174dfb331827dc7261
akhlas-jerno <akhlas@jerno.io>
2022-09-07 05:40:13 +0530

    latest

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ $( document ).ready(function() {
           sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
           sParameterName,
           i;
+      console.log(sPageURL)    
       let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
       sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
+      console.log(split_str)  
       for (i = 0; i < sURLVariables.length; i++) {
           sParameterName = sURLVariables[i].split('=');
           if (sParameterName[0] === sParam) {
@@ -14,11 +16,13 @@ $( document ).ready(function() {
           }
       }
   };
-    const accessToken = getUrlParameter('access_token');
+  
+  const accessToken = getUrlParameter('access_token');
 
-    let client_id = 'da267bfa27f24526a2c41fc0f02ca75c';
+  let client_id = 'da267bfa27f24526a2c41fc0f02ca75c';
 
-    let redirect_uri = "https%3A%2F%2Fmusix-play.herokuapp.com"; 
+    // let redirect_uri = "https%3A%2F%2Fmusix-play.herokuapp.com"; 
+  let redirect_uri = "http%3A%2F%2F127.0.0.1%3A5500"
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
    
